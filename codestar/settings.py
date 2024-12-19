910fdbea3a4607520b0914bb473a12d9408094a1
Daniel Beckett <dan-beckett@hotmail.co.uk>
2024-12-19 12:06:04 +0000

    Add: Gunicorn and procfile config

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6oq!m!zu7lnq35!*lge8==844bin6_nylu_-@vmt)!2uslcsrf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
