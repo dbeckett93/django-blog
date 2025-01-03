5e9befa0b616e91b4adfb03aa5540ddf21f3de34
Daniel Beckett <dan-beckett@hotmail.co.uk>
2025-01-03 16:27:20 +0000

    Refactor: update runtime files, add profile image to About model, and enhance forms and views for collaboration

--- about/views.py
+++ about/views.py
@@ -7,24 +7,31 @@ from .forms import CollaborateForm
 
 def about_me(request):
     """
-    Renders the About page
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+    
+    **Template**
+    :template:`about/about.html`
     """
-    about = About.objects.all().order_by('-updated_on').first()
+
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
-            collaborate_request = collaborate_form.save(commit=False)
-            collaborate_request.read = False
-            collaborate_request.save()
+            collaborate_form.save()
             messages.add_message(
                 request, messages.SUCCESS,
-                'Collaboration request received! I endeavor to respond within 2 working days.'
+                'Collaboration request received! I endeavour to respond within 2 working days.'
             )
-            collaborate_form = CollaborateForm()
-        else:
-            collaborate_form = CollaborateForm()
-    else:
-        collaborate_form = CollaborateForm()
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
